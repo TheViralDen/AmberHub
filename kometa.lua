e5ff2b01f365e158ce8dd9d6b9990d832cf5845a
TheViralDen <118555861+TheViralDen@users.noreply.github.com>
2023-03-12 03:40:45 +0000

    Update kometa.lua

--- kometa.lua
+++ kometa.lua
@@ -1,6 +1,6 @@
 getgenv().APIEnabled = true
 local api = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheViralDen/AmberHub/main/api.lua"))()
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 local bssapi = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheViralDen/AmberHub/main/bssapi.lua "))()
 
 if not isfolder("AmberHub") then makefolder("AmberHub") end
